b412d3db6b2f881d6d837ad1e54ac830fae88acc
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-06-23 11:59:57 +0200

    new index grid

--- app/application/route.js
+++ app/application/route.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 
 const {Route} = Ember;
 
-export default Route.extend({});
+export default Route.extend({
+	layout: Ember.inject.service(),
+
+	model(params) {
+		return this.store.query('post', {filter: {category_name: params.category, term: params.tag}});
+	}
+});
