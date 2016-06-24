1107b7e53f11d02a24da75cb45d7563e38849ae7
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-06-24 09:09:32 +0200

    detail + navigation + teaser

---
+++ app/detail/controller.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+const {$} = Ember;
+
+export default Ember.Controller.extend({
+
+	init() {
+		// console.log('init');
+		// $(() => {
+		// 	$('.Device').fadeIn(500);
+		// });
+	}
+});
