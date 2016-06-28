128c76f6cd445a21ae5180c537ab3cf126fae74c
Kimberly Gessner <kimberlygessner@yahoo.de>
2016-06-29 01:48:54 +0200

    detail scrollmagic action

--- app/detail/controller.js
+++ app/detail/controller.js
@@ -1,3 +1,9 @@
+/* global TimelineMax, TweenMax, Linear, Bounce */
+import gsap from 'npm:gsap';
+import {TweenMax} from 'npm:gsap';
+import ScrollMagic from 'npm:scrollmagic';
+import gsapAnimation from 'npm:scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
+import ScrollMagicIndicators from 'npm:scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 import Ember from 'ember';
 
 const {$} = Ember;
@@ -9,5 +15,16 @@ export default Ember.Controller.extend({
 		// $(() => {
 		// 	$('.Device').fadeIn(500);
 		// });
+
+		// init controller
+		const pipe = $('.Detail');
+		var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: 0, duration: "200%"}});
+
+		// build scenes
+		new ScrollMagic.Scene({triggerElement: pipe})
+			.setTween(".Detail-wrap", {y: "80%", ease: Linear.easeNone})
+			.addIndicators()
+			.addTo(controller);
 	}
+
 });
