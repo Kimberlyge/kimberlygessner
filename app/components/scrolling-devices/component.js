9fcc1b7332e0a937fe27b74bd8628aec3dfa8599
Kimberly Gessner <kimberlygessner@yahoo.de>
2017-01-28 17:50:23 +0100

    remove detail pages

--- app/components/scrolling-devices/component.js
+++ app/components/scrolling-devices/component.js
@@ -1,25 +1,25 @@
-/* global TimelineMax, TweenMax, Linear, Bounce */
-import gsap from 'npm:gsap';
-import {TimelineMax} from 'npm:gsap';
-import Ember from 'ember';
+// /* global TimelineMax, TweenMax, Linear, Bounce */
+// import gsap from 'npm:gsap';
+// import {TimelineMax} from 'npm:gsap';
+// import Ember from 'ember';
 
-const {Component, $, on} = Ember;
+// const {Component, $, on} = Ember;
 
-export default Component.extend({
+// export default Component.extend({
 
-	sticky: on('didRender', function() {
-		console.log('yo');
-		var deviceMobile = $('.Device--mobile');
-		var detailBody = $('.DetailBody');
-		var deviceTablet = $('.Device--tablet');
+// 	sticky: on('didRender', function() {
+// 		console.log('yo');
+// 		var deviceMobile = $('.Device--mobile');
+// 		var detailBody = $('.DetailBody');
+// 		var deviceTablet = $('.Device--tablet');
 
-		console.log(detailBody);
+// 		console.log(detailBody);
 
-		var tl = new TimelineMax();
+// 		var tl = new TimelineMax();
 
-		tl.fromTo(detailBody, 0.5 {autoAlpha: 0}, {autoAlpha: 1});
-		tl.to(deviceMobile, 1.5 {autoAlpha: 0});
-		tl.fromTo(deviceTablet, 3.5 {autoAlpha: 0}, {autoAlpha: 1});
-	});
+// 		tl.fromTo(detailBody, 0.5 {autoAlpha: 0}, {autoAlpha: 1});
+// 		tl.to(deviceMobile, 1.5 {autoAlpha: 0});
+// 		tl.fromTo(deviceTablet, 3.5 {autoAlpha: 0}, {autoAlpha: 1});
+// 	});
 
-});
+// });
